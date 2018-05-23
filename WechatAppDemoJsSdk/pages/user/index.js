5e2a543e889f3efbc3a3764458ab84a7ca297d6d
kesixin <462369233@qq.com>
2018-05-23 18:53:16 +0800

    user action

--- WechatAppDemoJsSdk/pages/user/index.js
+++ WechatAppDemoJsSdk/pages/user/index.js
@@ -1,7 +1,10 @@
 var grids = [
-  {"name":"登录","ico":"login.png","url":"../login/index"},
-  {"name":"注册","ico":"reg.png","url":"../register/index"},
-
+  {"name":"登录","ico":"login.png","url":"../user/login/index"},
+  {"name":"注册","ico":"reg.png","url":"../user/register/index"},
+  {"name": "验证码登录", "ico":"sms.png","url":"../user/smsLogin/index"},
+  {"name":"获取用户登录信息","ico":"reg.png","url":"../user/userInfo/index"},
+  {"name": "验证Email", "ico": "sms.png", "url": "../user/checkEmail/index" },
+  {"name": "密码重置", "ico": "pswdreset.jpg", "url": "../user/checkEmail/index" }
 ];
 
 Page({
--- WechatAppDemoJsSdk/pages/user/login/index.js
+++ WechatAppDemoJsSdk/pages/user/login/index.js
@@ -1,10 +1,11 @@
 // pages/login/index.js
-var Bmob = require("../../dist/Bmob-1.4.2.min.js");
-var common = require("../../utils/common.js")
+var Bmob = require("../../../dist/Bmob-1.4.2.min.js");
+var common = require("../../../utils/common.js")
 Page({
   formsubmit:function(event){
     Bmob.User.login(event.detail.value.username,event.detail.value.password).then(res=>{
       common.showTip("登录成功");
+      wx.setStorageSync("objectId", res['objectId']);
       setTimeout(function () {
         wx.switchTab({
           url: '/pages/user/index',
@@ -14,5 +15,13 @@ Page({
       console.log(err);
       common.showTip("登录失败", "loading");
     })
+  },
+  getUserInfo:function(){
+    let current = Bmob.User.current();
+    if(current){
+      console.log(current);
+    }else{
+      common.showTip("请先登录","loading");
+    }
   }
 })
